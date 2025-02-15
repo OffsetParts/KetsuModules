a022ba9a0ab73b1194edc8675c3749d1329e54ee
Nope Nata <44343694+OffsetParts@users.noreply.github.com>
2025-02-15 05:31:11 -0500

    To the folly let there be light

--- Modules/AsuraScans/Chapters/Chapters.js
+++ Modules/AsuraScans/Chapters/Chapters.js
@@ -22,10 +22,10 @@ function Commands(commandName, params) {
 	this.params = params;
 }
 
-function JavascriptConfig(removeJavascript, loadInWebView, javaScript) {
+function JavascriptConfig(removeJavascript, loadInWebView) {
 	this.removeJavascript = removeJavascript;
 	this.loadInWebView = loadInWebView;
-	this.javaScript = javaScript;
+	this.javaScript = '';
 }
 
 function KeyValue(key, value) {
@@ -37,9 +37,9 @@ function Package(images) {
 	this.images = images;
 }
 
-var id = 'ketsu-final-data'
-let ketsu = document.createElement('div')
-ketsu.setAttribute('id', id)
+var id = 'ketsu-final-data';
+let ketsu = document.createElement('div');
+ketsu.setAttribute('id', id);
 
 var emptyKeyValue = [ new KeyValue( '', '' ) ];
 let emptyExtra = new Extra([new Commands('', emptyKeyValue)], emptyKeyValue);
@@ -51,44 +51,34 @@ function cleanUrl(url) {
 	return (url).trim();
 }
 
-function cleanText(str) {
-	return str.replace(/[\\n\\t]/g, '').trim();
+function quickRequest(url) {
+	return new ModuleRequest(cleanUrl(url), 'GET', emptyKeyValue, null);
 }
 
-function quickRequest(url, clean) {
-	if (clean == true) {
-		return new ModuleRequest(cleanUrl(url), 'get', emptyKeyValue, null);
-	} else if (clean == false || clean == null) {
-		return new ModuleRequest(cleanText(url), 'get', emptyKeyValue, null);
-	}
-}
-
-
-let deploySource = () => (Array.from(document.querySelectorAll('[alt*=\"chapter page\"]')).map((images) => (
-	new ModuleRequest(images.src,'GET',emptyKeyValue,undefined)
-)))
+let deploySource = () => (
+	Array.from(document.querySelectorAll('[alt*=\"chapter page\"]')).map((img) => (quickRequest(img.src)))
+);
 
 let finish = (images) => {
-	var output =  new Output(undefined,images,undefined)
-	var chaptersObject = new MChapters( new ModuleRequest( '', '', emptyKeyValue, null ), emptyExtra, new JavascriptConfig( false, false ), output);
+	var chaptersObject = new Chapters( new ModuleRequest( '', '', emptyKeyValue, null ), emptyExtra, new JavascriptConfig( false, false ), new Package(images));
 	var finalJson = JSON.stringify( chaptersObject );
-	document.body.querySelectorAll('#' + id).forEach((el) => (el.remove()))
-	document.body.prepend(ketsu)
+	document.body.querySelectorAll('#' + id).forEach((el) => (el.remove()));
+	document.body.prepend(ketsu);
 	ketsu.innerHTML = finalJson;
 	window.webkit.messageHandlers.EXECUTE_KETSU_ASYNC.postMessage('');
 }
 
-let tries = 0
-let maxTries = 15
+let tries = 0;
+let maxTries = 15;
 let interval = setInterval(() => {
-	let makeup = deploySource()
-	if (makeup.length > 0) {
-		clearInterval(interval)
-		finish(images)
-		return
-	}
-	tries++
-	if (tries > maxTries) {
-		finish([]);
-	}
-},300)
+    let initial = deploySource();
+    if (initial.length > 0) {
+        clearInterval(interval);
+        finish(initial);
+        return;
+    }
+    tries++;
+    if (tries > maxTries) {
+        finish([]);
+    }
+}, 300);
